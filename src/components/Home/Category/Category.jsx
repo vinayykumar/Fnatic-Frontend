bdd0a02a2578c3ed1473f34653b944869fe424c0
Vinay Kumar <Vinayy1709@gmail.com>
2024-05-30 01:41:09 +0530

    added hero section

---
+++ src/components/Home/Category/Category.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Category = () => {
+  return (
+    <div>
+      <div className='flex justify-center'>
+        <div>
+            <div className='flex flex-col gap-4 justify-center'>
+                <img className='w-24 h-24' src="../../../assets/products/category/headphones.svg" alt="Meowy" />
+                <p>Headphones</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Category
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Category from './Category/Category';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className='flex flex-col gap-8 w-full h-ful'>
+      <Category></Category>
+      <div className='w-full h-96 p-4 lg:p-0'>
+        <div className='relative bg-black h-96 z-0 rounded rounded-lg max-h-96'>
+          <div className='absolute inset-0 overflow-hidden z-10'>
+            <video className='w-full h-96 object-cover z-10 rounded-sm object-bottom' autoPlay loop muted>
+              <source src="https://player.vimeo.com/progressive_redirect/playback/869168512/rendition/1080p/file.mp4?loc=external&oauth2_token_id=1505220741&signature=9f5ab0ee1ce2ccb7deb30e038daaef584edd7df7e068b3a483c71bcf01c89342" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
