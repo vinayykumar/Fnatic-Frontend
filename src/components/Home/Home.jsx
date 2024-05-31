4210847a53a1c7b2a9fa06c806fe6cf588d9a6b2
Vinay Kumar <Vinayy1709@gmail.com>
2024-06-01 02:44:53 +0530

    added level_up section in homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,18 +3,14 @@ import Category from './Category/Category';
 
 const Home = () => {
   return (
-    <div className='flex flex-col gap-8 w-full h-ful'>
+    <div className='flex flex-col gap-8 w-full h-full'>
       <Category></Category>
-      {/* <div className='w-full h-96 p-4 lg:p-0'>
-        <div className='relative bg-black h-96 z-0 rounded rounded-lg max-h-96'>
-          <div className='absolute inset-0 overflow-hidden z-10'>
-            <video className='w-full h-96 object-cover z-10 rounded-sm object-bottom' autoPlay loop muted>
-              <source src="https://player.vimeo.com/progressive_redirect/playback/869168512/rendition/1080p/file.mp4?loc=external&oauth2_token_id=1505220741&signature=9f5ab0ee1ce2ccb7deb30e038daaef584edd7df7e068b3a483c71bcf01c89342" type="video/mp4" />
-            </video>
-          </div>
-        </div>
-      </div> */}
-      
+      <div className='flex flex-col items-center w-full'>
+        <h1 style={{ fontFamily: 'DrukeWide' }} className='text-3xl text-center md:text-4xl md:w-96 '>LEVEL UP YOUR GAME.</h1>
+        <img src="https://cf-img.fnatic.com/cdn-cgi/image/dpr=1,fit=contain,format=auto,height=3200,width=1280,trim=266;0;267;0/https://cdn.sanity.io/images/5gii1snx/production/58717b85213e0cbf8f8b1663204eeb6bb2b60c5c-1600x1600.png" alt="Keyboard" className='w-96 h-auto lg:w-3/4 md:mt-[-170px]' />
+        <p className='text-center mt-[-170px] w-96'>Esports gear is about simplicity, comfort and reliability - it's exactly what you need to perform and nothing else</p>
+        <button className='mt-4 w-36 h-12 bg-black text-white rounded rounded-lg p-2 text-sm mt-0'>See Fnatic Gear</button>
+      </div>
     </div>
   );
 }
